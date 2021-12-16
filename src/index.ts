b532b34e81f28a6decab9f36bd1ae4e685f4540f
mojahige <mojahige@gmail.com>
2021-12-16 10:42:22 +0900

    Add getTargets

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
 import { contentLoaded } from "./contentLoaded";
 import type { Application } from "@hotwired/stimulus";
 
+export function getTargets() {
+  return document.querySelectorAll<HTMLElement>(
+    "[data-controller][lazy-controller]"
+  );
+}
+
 export function start(application: Application) {
   if (application == undefined) {
     throw new Error("application is undefined");
   }
 
   contentLoaded().then(() => {
-    document
-      .querySelectorAll<HTMLElement>("[data-controller][lazy-controller]")
-      .forEach((target) => {
-        const name = target.dataset.controller;
+    getTargets().forEach((target) => {
+      const name = target.dataset.controller;
 
-        if (typeof name == "string") {
-          import(`/${name}_controller.js`).then((module) => {
-            application.register(name, module.default);
-          });
-        }
-      });
+      if (typeof name == "string") {
+        import(`/${name}_controller.js`).then((module) => {
+          application.register(name, module.default);
+        });
+      }
+    });
   });
 }
